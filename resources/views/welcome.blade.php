4e80382366031e8e2b897bc72f8e0b19063642da
Cory Monteleone <cmontele@rcgc.edu>
2019-01-10 14:57:31 -0500

    Updated splash screen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,16 +81,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Rowan College
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://portal.rcgc.edu">Portal</a>
+                    <a href="https://rcgc.edu">Website</a>
+                    <a href="https://portal-mail.rcgc.edu">Email</a>
+                    <a href="https://mycourses.rcgc.edu">eLearning</a>
                 </div>
             </div>
         </div>
